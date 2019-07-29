14fb072c56dfb01adba8265ac8361c49e28260de
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-29 14:53:46 +0530

    ResponseData added and entity change

---
+++ src/main/java/com/management/stock/model/ResponseData.java
@@ -0,0 +1,18 @@
+package com.management.stock.model;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseData {
+
+	private String responseMessage;
+	private Map<Integer, String> status;
+	private Object data;
+
+}
