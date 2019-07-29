0aa8935a1855b9f30b79497f189b9e3cdaca5704
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-29 14:15:46 +0530

    StockModel and StockService added

---
+++ src/main/java/com/management/stock/model/StockModel.java
@@ -0,0 +1,26 @@
+package com.management.stock.model;
+
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockModel 
+{
+	
+	private String  symbol;
+	private String  name;
+	private String  type;
+	private String  region;
+	private LocalTime  marketOpen;
+	private LocalTime  marketClose;
+	private String  timezone;
+	private String  currency;
+	private Double  matchScore;
+	private Double  price;
+
+}
